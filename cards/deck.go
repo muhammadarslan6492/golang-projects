479d38fe9cd54fe0285689e830cbee86fcc4cb01
muhammad arslan <muhammadarslan6492@gmail.com>
2023-07-04 23:11:16 +0300

    create new Deck function

--- cards/deck.go
+++ cards/deck.go
@@ -2,7 +2,25 @@ package main
 
 import "fmt"
 
+
+// custome type decorator
 type deck []string
+
+// create a function for add new deck and returs
+
+func newDexk() deck  {
+	cards:= deck{}
+
+	cardSuits:= []string {"Spades", "Diamonds", "Hearts","Clubs"}
+	cardValue:= []string {"Ace", "Two", "Three", "Four"}
+
+	for _, suit:=range cardSuits {
+		for _, value:=range cardValue {
+			cards = append(cards, value + " of " + suit)
+		}		
+	}
+	return cards
+}
 
 func (d deck) print() {
 
--- cards/main.go
+++ cards/main.go
@@ -2,14 +2,8 @@ package main
 
 func main() {
 
-	cards := deck{"Ace of Dimonds", newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newDexk()
 
 	cards.print()
 
-}
-
-func newCard() string {
-
-	return "Five of Dimonds"
 }
